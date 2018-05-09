11304c59cafd652b95b47499911d5bf3126cfd64
amalgum <37487323+amalgum@users.noreply.github.com>
2018-05-09 22:36:07 +0200

    Setup and add static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Laracarte</title>
+    <!-- use mobile(ressponsive) -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- google font-->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <!-- font awesome-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+
+
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+  </head>
+  <body>
+    @include('layouts/partials/_nav')
+ @yield('content')
+ @include('layouts/partials/_footer')
+
+ <script src="//code.jquery.com/jquery.min.js">
+
+ </script>
+ <!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<style>
+  body{
+    font-family: 'Open Sans',Helvetica,Arial,sans-serif;
+  }
+  footer{
+  
+    margin: 4em 0;
+  }
+</style>
+  </body>
+</html>
---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="text-center">
+  <p>
+     &copy;{{date('Y')}} &middot;  Laracarte by <a href="https://twitter.com/estmo">@estmo</a>&
+  dash; A <a href="https://laramp.com">Laramp</a> clone app.
+ </p>
+ <p> <strong> this app has been built for learning purposes.</strong></p>
+</footer>
