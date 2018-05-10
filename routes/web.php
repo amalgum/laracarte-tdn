1e4a4a17b04e2cf3b4c05fa1ae7f30605f99ac75
amalgum <37487323+amalgum@users.noreply.github.com>
2018-05-10 03:27:03 +0200

    Add contact page

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/about', [
   'as'=> 'about_path',
   'uses'=> 'PagesController@about'
 ]);
+Route::get('/contact', [
+  'as'=> 'contact_path',
+  'uses'=> 'ContactsController@create'
+]);
